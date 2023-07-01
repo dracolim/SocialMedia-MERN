088c4741eeace8b0a8e051743b0f6322a69e9c14
CherylLim <85498185+dracolim@users.noreply.github.com>
2023-07-01 23:39:03 +0800

    updated the themeSettings

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+
+const FlexBetween = styled(Box)({
+    // pass in css properties
+
+})
--- client/src/index.js
+++ client/src/index.js
@@ -1,43 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import authReducer from "./state";
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
   persistStore,
-  persisteReducer,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER
+  REGISTER,
 } from "redux-persist";
-import storage from 'redux-persist/lib/storage'; // user info will be saved in local storage, unless they clear their cache
-import { PersistGate } from 'redux-persist/integration/react';
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig = { key: "root" , storage , version:1};
-const persistReducer = persisteReducer(persistConfig , authReducer);
-const store = configureStore ({
-  reducer: persistReducer,
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware ({
+    getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH , REHYDRATE, PAUSE,PERSIST,PURGE,REGISTER],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-  }),
-})
+    }),
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store = {store}>
-      <PersistGate loading = {null} persistor={persistStore(store)}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
