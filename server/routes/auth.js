fec49c5a9511ee00611e6a87920f24d19b50888f
CherylLim <85498185+dracolim@users.noreply.github.com>
2023-07-01 17:43:35 +0800

    set up user controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ import { login } from "../controllers/auth.js";
 
 const router = express.Router();
 
+/* auth/login => "auth/" set in index.js */
 router.post("/login" , login);
 
 export default router;
